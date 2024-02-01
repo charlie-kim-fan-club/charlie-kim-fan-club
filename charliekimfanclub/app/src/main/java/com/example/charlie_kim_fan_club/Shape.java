be4731f6502b1dc7771b4365ca46ad6025df05a9
Karan0Sidhu <kjsidhu@ualberta.ca>
2024-02-01 12:56:42 -0700

    Update Shape.java

--- charliekimfanclub/app/src/main/java/com/example/charlie_kim_fan_club/Shape.java
+++ charliekimfanclub/app/src/main/java/com/example/charlie_kim_fan_club/Shape.java
@@ -2,6 +2,7 @@ package com.example.charlie_kim_fan_club;
 
 public abstract class Shape {
     int x,y;
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
     public Shape(int x,int y){
         this.x = x;
         this.y = y;
